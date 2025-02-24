573a18802e71b42713aed2e7f7fa9d7c7cd07578
kenny <kennygarcia15@csu.fullerton.edu>
2025-02-23 22:18:31 -0800

    Implemented local storage display

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,7 +4,7 @@ const Hero = () => {
     <header className="w-full flex justify-center items-center flex-col">
         <nav className="flex justify-between items-center w-full mb-10 pt-3">
             <img src={logo} alt="sumz_logo" className="w-28 object-contain" />
-            <button type="button" onClick={() => window.open('https://github.com/bebopkenny')} className="black_btn">
+            <button type="button" onClick={() => window.open('https://github.com/bebopkenny/AI-Summarizer')} className="black_btn">
                 GitHub
             </button>
         </nav>
@@ -12,8 +12,7 @@ const Hero = () => {
             <span className="orange_gradient">OpenAI GPT-4</span>
         </h1>
         <h2 className="desc">
-            Simplify your reading with Summize, an open-source article summarizer that transforms lengthy articles into 
-            clear and concise summaries
+            Streamline your reading with Summize, an open-source tool that converts long articles into clear, concise summaries
         </h2>
     </header>
   )
